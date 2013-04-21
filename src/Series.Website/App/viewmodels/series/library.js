21c12f44f9ae22476329843383e867c969a3a558
Guillaume Leroi <leroi.g@gmail.com>
2013-04-21 19:16:14 +0200

    basic mockup of library screen

--- src/Series.Website/App/viewmodels/series/library.js
+++ src/Series.Website/App/viewmodels/series/library.js
@@ -1,22 +1,57 @@
 define(["require", "exports"], function(require, exports) {
     (function (Library) {
-        Library.displayName = "Library";
-        Library.followedSeries = ko.observableArray();
+        var Episode = (function () {
+            function Episode(title, season, opus) {
+                this.title = ko.observable();
+                this.opus = ko.observable();
+                this.season = ko.observable();
+                this.title(title);
+                this.season(season);
+                this.opus(opus);
+            }
+            return Episode;
+        })();        
         var Serie = (function () {
             function Serie(id, name) {
                 this.id = ko.observable();
                 this.name = ko.observable();
+                this.episodes = ko.observableArray();
                 this.id(id);
                 this.name(name);
             }
             return Serie;
         })();        
+        Library.displayName = "Library";
+        Library.followedSeries = ko.observableArray();
+        Library.foundedSeries = ko.observableArray();
+        Library.searchTerm = ko.observable();
+        function search() {
+            Library.foundedSeries([
+                makeSerie("Once upon a time", 15), 
+                makeSerie("The walking dead", 16), 
+                makeSerie("Misfits", 6), 
+                makeSerie("Community", 17), 
+                
+            ]);
+        }
+        Library.search = search;
+        function makeSerie(title, epCount) {
+            var serie = new Serie(0, title);
+            var result = [];
+            for(var i = 1; i <= epCount; i++) {
+                var ep = new Episode(title + " " + i, 1, i);
+                result.push(ep);
+            }
+            serie.episodes(result);
+            return serie;
+        }
         function init() {
             Library.followedSeries([
-                new Serie(0, "0000"), 
-                new Serie(1, "0001"), 
-                new Serie(2, "0002"), 
-                new Serie(3, "0003")
+                makeSerie("Californication", 15), 
+                makeSerie("How i met your mother", 16), 
+                makeSerie("Big Bang Theory", 6), 
+                makeSerie("Game of Throne", 17), 
+                
             ]);
         }
         init();
--- src/Series.Website/App/viewmodels/series/library.ts
+++ src/Series.Website/App/viewmodels/series/library.ts
@@ -2,26 +2,63 @@
 
 export module Library {
 
-    export var displayName: string = "Library";
-    export var followedSeries: KnockoutObservableArray = ko.observableArray();
+    class Episode {
+        public title: KnockoutObservableString = ko.observable();
+        public opus: KnockoutObservableNumber = ko.observable();
+        public season: KnockoutObservableNumber = ko.observable();
 
+        constructor (title: string, season: number, opus: number) {
+            this.title(title);
+            this.season(season);
+            this.opus(opus);
+        }
+    }
 
     class Serie {
         public id: KnockoutObservableNumber = ko.observable();
         public name: KnockoutObservableString = ko.observable();
+        public episodes: KnockoutObservableArray = ko.observableArray();
 
-        constructor(id: number, name: string) {
+        constructor (id: number, name: string) {
             this.id(id);
             this.name(name);
         }
     }
 
+
+    // Main library view model
+
+    export var displayName: string = "Library";
+    export var followedSeries: KnockoutObservableArray = ko.observableArray();
+    export var foundedSeries: KnockoutObservableArray = ko.observableArray();
+    export var searchTerm: KnockoutObservableString = ko.observable();
+
+    export function search() {
+        foundedSeries([
+            makeSerie("Once upon a time", 15),
+            makeSerie("The walking dead", 16),
+            makeSerie("Misfits", 6),
+            makeSerie("Community", 17),
+        ]);
+    }
+
+    function makeSerie(title: string, epCount: number): Serie {
+        var serie = new Serie(0, title);
+        var result = [];
+        for (var i = 1; i <= epCount; i++) {
+            var ep = new Episode(title + " " + i, 1, i);
+            result.push(ep);
+        }
+        serie.episodes(result);
+        return serie;
+    }
+
     function init() {
         followedSeries([
-            new Serie(0, "0000"),
-            new Serie(1, "0001"),
-            new Serie(2, "0002"),
-            new Serie(3, "0003")
+            makeSerie("Californication", 15),
+            makeSerie("How i met your mother", 16),
+            makeSerie("Big Bang Theory", 6),
+            makeSerie("Game of Throne", 17),
         ]);
     }
 
